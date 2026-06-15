f6dfded897406e3c09e37e1657384e6a07061387
tmaeda11235 <tmaeda11235@gmail.com>
2026-06-15 17:51:36 +0900

    refactor: decompose EasyModeView and EasyModeSection components to satisfy 50-line limit

--- src/components/organisms/EasyModeView.tsx
+++ src/components/organisms/EasyModeView.tsx
@@ -83,77 +83,187 @@ export function EasyModeView({
         alertType={alertType}
         onRetryConnection={handleRetryConnection}
         onDismissAlert={handleDismissAlert}
-        onOpenGuide={() => {}} // Guide is not supported in Easy Mode
+        onOpenGuide={() => {}}
         isEasyMode={isEasyMode}>
-        {(minting.mintingItem || minting.variationBase) && (
-          <SimpleMintingView
-            mintingItem={minting.mintingItem}
-            editedSegments={minting.editedSegments}
-            setEditedSegments={minting.setEditedSegments}
-            onCancelMinting={() => {
-              minting.setMintingItem(null)
-              minting.setVariationBase(null)
-            }}
-            onSaveMintedCard={minting.handleSaveMintedCard}
-            suggestedKeywords={minting.suggestedKeywords}
-            selectedKeywords={minting.selectedKeywords}
-            setSelectedKeywords={minting.setSelectedKeywords}
-            customName={minting.customName}
-            setCustomName={minting.setCustomName}
-            selectedCategory={minting.selectedCategory}
-            setSelectedCategory={minting.setSelectedCategory}
-          />
-        )}
-        {activeDetailCard && (
-          <CardDetailView
-            card={activeDetailCard}
-            onClose={() => setActiveDetailCard(null)}
-            onInject={handleInjectPrompt}
-            onSave={handleSaveCardDetails}
-            setAlertType={setAlertType}
-            onCardSelect={async (cardId) => {
-              const targetCard = await getStyleCardById(cardId)
-              if (targetCard) {
-                setActiveDetailCard(targetCard)
-              } else {
-                addLog("Warning: Selected parent card could not be found.")
-              }
-            }}
-            onDelete={handleDeleteCard}
-          />
-        )}
-        {activeSimpleWorkbenchCard && (
-          <SimpleWorkbenchModal
-            card={activeSimpleWorkbenchCard}
-            onClose={() => setActiveSimpleWorkbenchCard(null)}
-            addLog={addLog}
-            setAlertType={setAlertType}
-          />
-        )}
-
-        {activeTab === "library" &&
-          (!isTargetSite ? (
-            <NonTargetSiteView onOpenMidjourney={onOpenMidjourney} isEmbedded />
-          ) : (
-            <LibraryTab
-              addLog={addLog}
-              setAlertType={setAlertType}
-              onOpenDetailCard={setActiveDetailCard}
-              onNavigateToWorkbench={() => {}} // Workbench navigation is disabled in Easy Mode
-              isEasyMode={isEasyMode}
-              onOpenSimpleWorkbench={setActiveSimpleWorkbenchCard}
-            />
-          ))}
-        {activeTab === "settings" && (
-          <SettingsTab
-            addLog={addLog}
-            onResetDb={handleResetDb}
-            isEasyMode={isEasyMode}
-            onToggleEasyMode={handleToggleEasyMode}
-            onNavigateToLibrary={() => setActiveTab("library")}
-          />
-        )}
+        <EasyModeMintingWrapper minting={minting} />
+        <EasyModeDetailWrapper
+          activeDetailCard={activeDetailCard}
+          setActiveDetailCard={setActiveDetailCard}
+          handleInjectPrompt={handleInjectPrompt}
+          handleSaveCardDetails={handleSaveCardDetails}
+          handleDeleteCard={handleDeleteCard}
+          setAlertType={setAlertType}
+          addLog={addLog}
+        />
+        <EasyModeWorkbenchWrapper
+          activeSimpleWorkbenchCard={activeSimpleWorkbenchCard}
+          setActiveSimpleWorkbenchCard={setActiveSimpleWorkbenchCard}
+          addLog={addLog}
+          setAlertType={setAlertType}
+        />
+        <EasyModeTabContent
+          activeTab={activeTab}
+          isTargetSite={isTargetSite}
+          onOpenMidjourney={onOpenMidjourney}
+          addLog={addLog}
+          setAlertType={setAlertType}
+          setActiveDetailCard={setActiveDetailCard}
+          setActiveSimpleWorkbenchCard={setActiveSimpleWorkbenchCard}
+          isEasyMode={isEasyMode}
+          handleToggleEasyMode={handleToggleEasyMode}
+          setActiveTab={setActiveTab}
+          handleResetDb={handleResetDb}
+        />
       </SidePanelLayout>
     </div>
   )
+}
+
+interface EasyModeMintingWrapperProps {
+  minting: any
+}
+
+function EasyModeMintingWrapper({ minting }: EasyModeMintingWrapperProps) {
+  if (!minting.mintingItem && !minting.variationBase) return null
+  return (
+    <SimpleMintingView
+      mintingItem={minting.mintingItem}
+      editedSegments={minting.editedSegments}
+      setEditedSegments={minting.setEditedSegments}
+      onCancelMinting={() => {
+        minting.setMintingItem(null)
+        minting.setVariationBase(null)
+      }}
+      onSaveMintedCard={minting.handleSaveMintedCard}
+      suggestedKeywords={minting.suggestedKeywords}
+      selectedKeywords={minting.selectedKeywords}
+      setSelectedKeywords={minting.setSelectedKeywords}
+      customName={minting.customName}
+      setCustomName={minting.setCustomName}
+      selectedCategory={minting.selectedCategory}
+      setSelectedCategory={minting.setSelectedCategory}
+    />
+  )
+}
+
+interface EasyModeDetailWrapperProps {
+  activeDetailCard: any
+  setActiveDetailCard: (card: any | null) => void
+  handleInjectPrompt: any
+  handleSaveCardDetails: any
+  handleDeleteCard: any
+  setAlertType: (type: any) => void
+  addLog: (log: string) => void
+}
+
+function EasyModeDetailWrapper({
+  activeDetailCard,
+  setActiveDetailCard,
+  handleInjectPrompt,
+  handleSaveCardDetails,
+  handleDeleteCard,
+  setAlertType,
+  addLog
+}: EasyModeDetailWrapperProps) {
+  if (!activeDetailCard) return null
+  return (
+    <CardDetailView
+      card={activeDetailCard}
+      onClose={() => setActiveDetailCard(null)}
+      onInject={handleInjectPrompt}
+      onSave={handleSaveCardDetails}
+      setAlertType={setAlertType}
+      onCardSelect={async (cardId) => {
+        const targetCard = await getStyleCardById(cardId)
+        if (targetCard) {
+          setActiveDetailCard(targetCard)
+        } else {
+          addLog("Warning: Selected parent card could not be found.")
+        }
+      }}
+      onDelete={handleDeleteCard}
+    />
+  )
+}
+
+interface EasyModeWorkbenchWrapperProps {
+  activeSimpleWorkbenchCard: any
+  setActiveSimpleWorkbenchCard: (card: any | null) => void
+  addLog: (log: string) => void
+  setAlertType: (type: any) => void
+}
+
+function EasyModeWorkbenchWrapper({
+  activeSimpleWorkbenchCard,
+  setActiveSimpleWorkbenchCard,
+  addLog,
+  setAlertType
+}: EasyModeWorkbenchWrapperProps) {
+  if (!activeSimpleWorkbenchCard) return null
+  return (
+    <SimpleWorkbenchModal
+      card={activeSimpleWorkbenchCard}
+      onClose={() => setActiveSimpleWorkbenchCard(null)}
+      addLog={addLog}
+      setAlertType={setAlertType}
+    />
+  )
+}
+
+interface EasyModeTabContentProps {
+  activeTab: string
+  isTargetSite: boolean
+  onOpenMidjourney: () => void
+  addLog: (log: string) => void
+  setAlertType: (type: any) => void
+  setActiveDetailCard: (card: any | null) => void
+  setActiveSimpleWorkbenchCard: (card: any | null) => void
+  isEasyMode: boolean
+  handleToggleEasyMode: (enabled: boolean) => void
+  setActiveTab: (tab: any) => void
+  handleResetDb: () => void
+}
+
+function EasyModeTabContent({
+  activeTab,
+  isTargetSite,
+  onOpenMidjourney,
+  addLog,
+  setAlertType,
+  setActiveDetailCard,
+  setActiveSimpleWorkbenchCard,
+  isEasyMode,
+  handleToggleEasyMode,
+  setActiveTab,
+  handleResetDb
+}: EasyModeTabContentProps) {
+  if (activeTab === "library") {
+    if (!isTargetSite) {
+      return (
+        <NonTargetSiteView onOpenMidjourney={onOpenMidjourney} isEmbedded />
+      )
+    }
+    return (
+      <LibraryTab
+        addLog={addLog}
+        setAlertType={setAlertType}
+        onOpenDetailCard={setActiveDetailCard}
+        onNavigateToWorkbench={() => {}} // Workbench navigation is disabled in Easy Mode
+        isEasyMode={isEasyMode}
+        onOpenSimpleWorkbench={setActiveSimpleWorkbenchCard}
+      />
+    )
+  }
+  if (activeTab === "settings") {
+    return (
+      <SettingsTab
+        addLog={addLog}
+        onResetDb={handleResetDb}
+        isEasyMode={isEasyMode}
+        onToggleEasyMode={handleToggleEasyMode}
+        onNavigateToLibrary={() => setActiveTab("library")}
+      />
+    )
+  }
+  return null
 }
